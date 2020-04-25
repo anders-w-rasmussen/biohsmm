c31ae1a02199a83d42138b9bf5fe72bf78853e59
anders-w-rasmussen <32580447+anders-w-rasmussen@users.noreply.github.com>
2020-04-25 19:21:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/anders-w-rasmussen/biohsmm",
-    packages=['biohsmm'],
+    packages=['biohsmm', 'biohsmm/util'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
